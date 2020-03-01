8f87fed3856664956f954f0e6408cf1c90711207
rifadahmed <rifad.ja@gmail.com>
2020-03-02 01:39:56 +0600

    30.Done

--- app.js
+++ app.js
@@ -11,27 +11,14 @@ app.set("view engine","ejs");
 //campgroundSchema
 var campgroundSchema= new mongoose.Schema({
     name:String,
-    image:String
+    image:String,
+    description:String
 })
 
 //campgroundMode
 var campground=mongoose.model("campground",campgroundSchema);
 
-// var x={
-//     name:"mohamaya",
-//         image:"https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-// }
-//create camp
-// campground.create(x,function(err,camp){
-//     if(err)
-//     {
-//         console.log("cant create");
-//     }
-//     else
-//     {
-//         console.log(camp);
-//     }
-// })
+
 
 
 
@@ -39,6 +26,7 @@ app.get("/",function(req,res){
     res.render("landing")
 })
 
+//index route
 app.get("/campgrounds",function(req,res){
    campground.find({},function(error,allCampgrounds){
        if(error)
@@ -46,16 +34,24 @@ app.get("/campgrounds",function(req,res){
            console.log("cant find");
        }
        else{
-        res.render("campgrounds",{campgrounds:allCampgrounds})
+        res.render("index",{campgrounds:allCampgrounds})
        }
    })
     
 })
 
+//new route
+app.get("/campgrounds/new",function(req,res){
+    res.render("new");
+})
+
+
+//create route
 app.post("/campgrounds",function(req,res){
     var name=req.body.name;
     var image=req.body.image;
-    var newCamp={name:name,image:image}
+    var description=req.body.description;
+    var newCamp={name:name,image:image,description:description}
     campground.create(newCamp,function(error,camp){
         if(error)
         {
@@ -70,9 +66,22 @@ app.post("/campgrounds",function(req,res){
    
 })
 
-app.get("/campgrounds/new",function(req,res){
-    res.render("new");
+//show route
+app.get("/campgrounds/:id",function(req,res){
+    var id=req.params.id
+    campground.findById(id,function(error,camp){
+        if(error)
+        {
+            console.log("cant find");
+        }
+        else{
+       
+            console.log(camp);
+            res.render("show",{camp:camp})
+    }
+    })
 })
+
 app.listen(3000,function(){
     console.log("server started");
 })
