a39512126ad35a050851246267bb39acbcc69079
rifadahmed <rifad.ja@gmail.com>
2020-03-01 23:44:44 +0600

    30: YelpCamp Adding Mongoose

--- app.js
+++ app.js
@@ -1,42 +1,73 @@
-var express=require("express");
-var app=express();
-var bodyParser=require("body-parser");
+var  express    =require("express"),
+     app        =express(),
+     bodyParser =require("body-parser"),
+     mongoose   =require("mongoose");
+     mongoose.connect('mongodb+srv://rifad:rifad2023@cluster1-vyqk2.mongodb.net/yelp_Camp?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true  })
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("view engine","ejs");
 
+//campgroundSchema
+var campgroundSchema= new mongoose.Schema({
+    name:String,
+    image:String
+})
 
-var campgrounds=[
-    {
-        name:"mohamaya",
-        image:"https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-    },
-    {
-        name:"mohamaya2",
-        image:"https://images.unsplash.com/photo-1515014631915-3527ef2a27cd?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-    },
-    {
-        name:"mohamaya3",
-        image:"https://images.unsplash.com/photo-1502218808493-e5fd26249efc?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-    }
-]
+//campgroundMode
+var campground=mongoose.model("campground",campgroundSchema);
+
+// var x={
+//     name:"mohamaya",
+//         image:"https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+// }
+//create camp
+// campground.create(x,function(err,camp){
+//     if(err)
+//     {
+//         console.log("cant create");
+//     }
+//     else
+//     {
+//         console.log(camp);
+//     }
+// })
+
+
+
 app.get("/",function(req,res){
     res.render("landing")
 })
 
 app.get("/campgrounds",function(req,res){
-   
-    res.render("campgrounds",{campgrounds:campgrounds})
+   campground.find({},function(error,allCampgrounds){
+       if(error)
+       {
+           console.log("cant find");
+       }
+       else{
+        res.render("campgrounds",{campgrounds:allCampgrounds})
+       }
+   })
+    
 })
 
 app.post("/campgrounds",function(req,res){
     var name=req.body.name;
     var image=req.body.image;
     var newCamp={name:name,image:image}
-    campgrounds.push(newCamp);
-    res.redirect("/campgrounds");
-    // console.log(name + image)
+    campground.create(newCamp,function(error,camp){
+        if(error)
+        {
+            console.log("cant create")
+        }
+        else
+        {
+            res.redirect("/campgrounds");
+        }
+    })
+    
+   
 })
 
 app.get("/campgrounds/new",function(req,res){
